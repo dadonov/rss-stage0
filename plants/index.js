5c1104670e3ddcf61a16b017cfb82da72e2ce134
dadonov <dadonov.igor@gmail.com>
2023-01-20 23:39:42 +0400

    feat: add media for smaller displays

--- plants/index.js
+++ plants/index.js
@@ -3,14 +3,20 @@
   const burger = document.querySelector(".header_burger");
   const menu = document.querySelector(".nav_menu");
   const closeMenu = document.querySelector(".nav_menu-close");
-  const link = document.querySelector(".nav_link");
+  const link = document.querySelector("nav_link");
+
   burger.addEventListener("click", () => {
     menu.classList.add("nav_menu_active");
   });
+
   closeMenu.addEventListener("click", () => {
     menu.classList.remove("nav_menu_active");
   });
-  link.addEventListener("click", () => {
-    menu.classList.remove("nav_menu_active");
+
+  menu.addEventListener("click", (event) => {
+    const target = event.target;
+    if (target.tagName === "A") {
+      menu.classList.remove("nav_menu_active");
+    }
   });
 })();
