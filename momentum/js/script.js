a4bd6fff11ca836b5f3540fc90705fcb068b5971
dadonov <dadonov.igor@gmail.com>
2023-02-11 18:51:54 +0400

    feat: add weather widget

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -2,12 +2,18 @@ const timeElement = document.querySelector(".time");
 const dateElement = document.querySelector(".date");
 const greetingElement = document.querySelector(".greeting");
 const timeOfDay = getTimeOfDay();
-const greeting = `Good ${timeOfDay},`;
 const username = document.querySelector(".name");
 const body = document.querySelector("body");
 let randomNumber;
 const slideNext = document.querySelector(".slide-next");
 const slidePrev = document.querySelector(".slide-prev");
+const city = document.querySelector(".city");
+const weatherIcon = document.querySelector(".weather-icon");
+const weatherDescription = document.querySelector(".weather-description");
+const weatherError = document.querySelector(".weather-error");
+const temperature = document.querySelector(".temperature");
+const wind = document.querySelector(".wind");
+const humidity = document.querySelector(".humidity");
 
 function showTime() {
   const date = new Date();
@@ -48,17 +54,25 @@ function getTimeOfDay() {
 }
 
 function showGreeting() {
+  const greeting = `Good ${timeOfDay},`;
   greetingElement.innerText = greeting;
 }
 
 function setLocalStorage() {
   localStorage.setItem("username", username.value);
+  localStorage.setItem("city", city.value);
 }
 window.addEventListener("beforeunload", setLocalStorage);
 
 function getLocalStorage() {
   if (localStorage.getItem("username")) {
     username.value = localStorage.getItem("username");
+  }
+  if (localStorage.getItem("city")) {
+    city.value = localStorage.getItem("city");
+    getWeather(); // have no idea if i need it there
+  } else {
+    city.value = "Minsk";
   }
 }
 window.addEventListener("load", getLocalStorage);
@@ -100,3 +114,19 @@ function getSlidePrev() {
 
 slideNext.addEventListener("click", getSlideNext);
 slidePrev.addEventListener("click", getSlidePrev);
+
+async function getWeather() {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=f0002bf3f73c9f1f545985301e5142ab&units=metric`;
+  const response = await fetch(url);
+  const weatherData = await response.json();
+
+  weatherIcon.className = "weather-icon owf";
+  weatherIcon.classList.add(`owf-${weatherData.weather[0].id}`);
+  weatherDescription.textContent = weatherData.weather[0].description;
+  temperature.textContent = `Temperature: ${Math.round(weatherData.main.temp)}°C`;
+  wind.textContent = `Wind speed: ${Math.round(weatherData.wind.speed)} m/s`;
+  humidity.textContent = `Humidity: ${Math.round(weatherData.main.humidity)}%`;
+}
+
+city.addEventListener("change", getWeather);
+// getWeather();
